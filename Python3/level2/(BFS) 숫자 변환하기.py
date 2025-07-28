254ea342adae9628808d736a3b8119d237ff4d9d
Yunji Jung <75105873+YunjiJung0105@users.noreply.github.com>
2025-07-28 20:54:51 +0900

    Update (BFS) 숫자 변환하기.py

--- Python3/level2/(BFS) 숫자 변환하기.py
+++ Python3/level2/(BFS) 숫자 변환하기.py
@@ -3,20 +3,20 @@ from collections import deque
 def solution(x, y, n):
     queue = deque()
     queue.append((x,0))
-    visited = set()   # visited 필요!
+    visited = set()
     
     while queue:
         now, step = queue.popleft()
         
-        if now > y or now in visited:
-            continue     
-        visited.add(now)
-        
         if now == y:
             return step
-
-        for k in [now*3, now*2, now+n]:   # 순서 주의!
-            if k<=y and k not in visited:
-                queue.append((k, step+1))
-              
+        
+        if now > y or now in visited:   # 필요!!
+            continue
+        visited.add(now)
+        
+        for i in [now*3, now*2, now+n]:
+            if i <= y and i not in visited:  # 여기서 같은 숫자가 여러번 append될 수 있음
+                queue.append((i,step+1))
+            
     return -1
